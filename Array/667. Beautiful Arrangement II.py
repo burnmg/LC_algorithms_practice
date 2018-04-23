8cd70d77be0ea3755defac7c3ad7753496a4e0bf
burnmg <rlin1992@hotmail.com>
2018-04-23 23:00:23 +0800

    pc

---
+++ Array/667. Beautiful Arrangement II.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def constructArray(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        nums = [1] * n
+        
+        diff = k
+        for i in range(1, k+1):
+            if i % 2 == 1:
+                nums[i] = nums[i-1] + diff
+            else:
+                nums[i] = nums[i-1] - diff
+            diff -= 1
+        for i in range(k+1, n):
+            nums[i] = (i+1)
+                
+        return nums

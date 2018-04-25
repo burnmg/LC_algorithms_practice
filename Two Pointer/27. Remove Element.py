86dac1399ccfd6e92ecf6c8c4c3534b4fe3b849c
burnmg <rlin1992@hotmail.com>
2018-04-26 00:37:38 +0800

    1

---
+++ Two Pointer/27. Remove Element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i,j = 0, len(nums) - 1
+        while i <= j:
+            if nums[i] == val:
+                nums[i],nums[j] = nums[j],nums[i]
+                j -= 1
+            else:
+                i += 1
+        return i

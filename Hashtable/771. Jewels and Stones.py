8c7a48a64d0f516c494661b4fee29b784acd8341
burnmg <rlin1992@hotmail.com>
2018-05-04 02:34:28 +0800

    lc

---
+++ Hashtable/771. Jewels and Stones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        jewels = set(J)
+        return sum(map(lambda x:x in jewels, S))
+            

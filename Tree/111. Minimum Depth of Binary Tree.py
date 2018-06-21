3cc9cf4189bba1ac6786324153e42d0a1ef49636
burnmg <rlin1992@hotmail.com>
2018-06-21 17:02:40 +0800

    lc

--- Tree/111. Minimum Depth of Binary Tree.py
+++ Tree/111. Minimum Depth of Binary Tree.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution(object):
-    def minDepth(self, root):
+    def minDepth2(self, root):
         """
         :type root: TreeNode
         :rtype: int
@@ -23,4 +23,21 @@ class Solution(object):
             return 1 + left
         
         return 1 + min(left, right)  
+    
+    def minDepth(self, root):
         
+        if not root:
+            return 0
+        queue = collections.deque()
+        queue.append((1,root))
+        
+        while queue:
+            x = queue.popleft()
+            if not x[1].left and not x[1].right:
+                return x[0]
+            else:
+                if x[1].left:
+                    queue.append((x[0]+1, x[1].left))
+                if x[1].right:
+                    queue.append((x[0]+1, x[1].right))
+        return 0
